43e121f44066432ea4f1c34a1079b07c878abad9
Tharun R <rajtharun08@gmail.com>
2025-08-15 16:28:50 +0530

    initial commit with frontend and backend structure

---
+++ backend/src/test/java/com/amazonclone/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazonclone.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
